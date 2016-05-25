1864bc1cb6a666d1ac7998b02b05869d950dfa54
oleooo <oleooo@126.com>
2016-05-25 15:33:08 +0800

    add openid

--- app/models.py
+++ app/models.py
@@ -2,10 +2,23 @@ from app import db
 
 
 class User(db.Model):
-    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    # openid auth
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % self.nickname
--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from app import db, models
+import datetime
 
-u = models.User(nickname='join', email="test@test.com")
+# u = models.User(nickname='mks', email="test1@test2.com")
+#
+# #db.create_all()
+# db.session.add(u)
+# db.session.commit()
 
-#db.create_all()
-db.session.add(u)
+#
+u = models.User.query.get(1)
+p = models.Post(body='My first blog!', timestamp=datetime.datetime.utcnow(), user_id=u.id)
+
+db.session.add(p)
 db.session.commit()
