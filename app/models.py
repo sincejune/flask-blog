ee95d22169265680caf57eaba7107c5bf8484e1a
oleooo <oleooo@126.com>
2016-05-28 00:58:49 +0800

    update database

--- app/models.py
+++ app/models.py
@@ -25,12 +25,6 @@ class User(db.Model):
     def is_authenticated(self):
         return True
 
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
     def get_id(self):
         return str(self.id)
 
@@ -62,9 +56,51 @@ class User(db.Model):
 
 class Post(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
-    body = db.Column('body', db.String(140))
+    title = db.Column('title', db.String(30))
+    body = db.Column('body', db.String(3000))
     timestamp = db.Column('timestamp', db.DateTime)
     user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    update = db.Column('update', db.Integer)
 
     def __repr__(self):
         return '<Post %r>' % self.body
+
+
+class Favorite(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    post_id = db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
+    timestamp = db.Column('timestamp', db.DateTime)
+    collection_id = db.Column('collector_id', db.Integer, db.ForeignKey('collection.id'))
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.post_id
+
+
+class Collection(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    user_id = db.Column('id', db.Integer, db.ForeignKey('user.id'))
+    title = db.Column('title', db.String(30))
+    timestamp = db.Column('timestamp', db.DateTime)
+    total = db.Column('total', db.Integer)
+
+    def __repr__(self):
+        return '<Collection %r>' % self.total
+
+
+class Comment(db.Models):
+    id = db.Column('id', db.Integer, primary_key=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user_id'))
+    post_id = db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
+    reply_id = db.Column('reply_id', db.Integer)
+    content = db.Column('content', db.String(300))
+    timestamp = db.Column('timestamp', db.DateTime)
+
+    def __repr__(self):
+        return '<Comment %r>' % self.content
+
+
+class Tag(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user_id'))
+    content = db.Column('content', db.String(300))
+    # TODO
